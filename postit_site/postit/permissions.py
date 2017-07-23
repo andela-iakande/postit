3ef2df39f92ec8cf3ed5aa96a790e4940c86e0db
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-07-23 01:08:06 +0100

    feature:User-should-be-able-to-paginate[Delivers#149352877]

--- postit_site/postit/permissions.py
+++ postit_site/postit/permissions.py
@@ -5,13 +5,12 @@ class IsOwnerOrReadOnly(BasePermission):
     message = 'You are not the owner of this post'
     my_safe_method = [ 'PUT', 'GET']
       
-    def has_permission(self, request, view):
-        if request.method in self.my_safe_method:
-            return True
-        return False
+    # def has_permission(self, request, view):
+    #     if request.method in self.my_safe_method:
+    #         return True
+    #     return False
 
     def has_object_permission(self, request, view, obj):
-       
         if request.method in SAFE_METHODS:
             return True
         return obj.user == request.user
--- postit_site/postit/views.py
+++ postit_site/postit/views.py
@@ -1,73 +1,72 @@
 from django.db.models import Q
-from django.shortcuts import render
-from rest_framework import generics, permissions
-#from .permissions import IsAuthor
-from .serializers import (
-    PostDetailSerializer,
-    PostCreateUpdateSerializer,
-    PostListSerializer
-)
+from rest_framework import generics
+
+from .models import Post
+from .permissions import IsOwnerOrReadOnly
+
+from .serializers import ( 
+    PostListSerializer, 
+    PostDetailSerializer, 
+    PostCreateUpdateSerializer
+    )   
+
 from rest_framework.permissions import (
-    AllowAny,
+    AllowAny, 
     IsAuthenticated,
     IsAdminUser,
     IsAuthenticatedOrReadOnly,
-)
-
-from .permissions import IsOwnerOrReadOnly
-
-from .models import Post
-from django.contrib.auth.models import User
-
-# Create your views here.
-
-# postit/views.py
+    )
+from .pagination import LimitOffsetPagination, PostPageNumberPagination
 
 class PostCreateAPIView(generics.CreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
-    #permission_classes = (permissions.Is Authenticated,  IsAuthor)
-
+   
     def perform_create(self, serializer):
         """Save the post data when creating a new Post."""
-        serializer.save(user=self.request.user )
-
-class PostDeleteAPIView(generics.DestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer                
+        serializer.save(user=self.request.user)
 
 class PostDetailAPIView(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+
+class PostDeleteAPIView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'   
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly ] 
 
 class PostListAPIView(generics.ListAPIView):
-    """This class defines the create behavior of our rest api."""
+    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination
 
     def get_queryset(self, *args, **kwargs):
         queryset_list = Post.objects.all()
         query = self.request.GET.get("q")
-        if query:
+        if query: 
             queryset_list = queryset_list.filter(
                 Q(title__icontains=query)|
-                Q(text__icontains=query)
-            ).distinct()
+                Q(content__icontains=query)|
+                Q(user__first_name__icontains=query)|
+                Q(user__last_name__icontains=query)
+                ).distinct()
         return queryset_list    
 
-
-
+        
 class PostUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostCreateUpdateSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]      
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug' 
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         """Save the post data when creating a new Post."""
-        serializer.save(user=self.request.user )
+        serializer.save(user=self.request.user)    
 
 
         
-
-     
+    
