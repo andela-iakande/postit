062862cce4bc80a34342c1a000c0396fabeda2e2
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-07-27 15:46:55 +0100

    Add whitenoise

--- postit_site/postit_site/settings.py
+++ postit_site/postit_site/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
